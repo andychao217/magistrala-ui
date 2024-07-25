f65448c58bc905f7f607d089ec788698d25980d4
Andy Chao <zhaoqing@spon.com.cn>
2024-07-25 16:39:03 +0800

    1、统一代码格式； 2、增加获取设备信息的mqtt解析，并且将新的设备信息保存至数据库

--- ui/web/static/js/websocket.js
+++ ui/web/static/js/websocket.js
@@ -1,43 +1,47 @@
-let ws = null;
-const hostName = getHostname(window.location);
+let ws = null, 
+    allThingsListWebsocket = []
+const hostNameWebsocket = getHostnameWebsocket(window.location),
+      protocolWebsocket = window.location.protocol,
+      portSocketBridge =  sessionStorage.getItem("socketBridgePort") || "63001",
+      userInfoStrWebsocket = sessionStorage.getItem("userInfo"),
+      userInfoWebsocket  = JSON.parse(userInfoStrWebsocket);
 
-function getHostname(url) {
+// 获取当前网页的域名
+function getHostnameWebsocket(url) {
     const parsedUrl = new URL(url);
     return parsedUrl.hostname;
 }
 
+// 建立websocket连接
 function connectWebSocket(host, port, defaultChannelId) {
     // 假设WebSocket服务器在ws://your-websocket-server-url
     ws = new WebSocket(`ws://${host}:${port}/websocket`);
 
+    // 监听WebSocket的open事件，连接成功时给后台发一些参数，后台去建立mqtt连接
     ws.onopen = function (event) {
         console.log("WebSocket is open now.");
-        // 可以在这里发送初始消息等
-        // getChannelsAndSendMessage(host)
         const topics = [
             `channels/${defaultChannelId}/messages/common/app`,
-            `channels/${defaultChannelId}/messages/common/device`
+            `channels/${defaultChannelId}/messages/common/device`,
+            `channels/${defaultChannelId}/messages/web_${defaultChannelId}`
         ];
         const message = { 
             topics: topics, 
-            host: hostName, 
+            host: hostNameWebsocket, 
             thingSecret: "platform" + defaultChannelId, 
             message: "connect" 
         };
         ws.send(JSON.stringify(message));
     };
 
-    ws.onmessage = function (event) {
-        console.log("Received data from server: ", event.data);
-        // 处理从服务器接收到的数据
-        const url = window.location.href;
+    // 监听WebSocket的message事件，后台发来消息时触发
+    ws.onmessage = function (event) {// 处理从服务器接收到的数据
+        const url = window.location.href, 
+              data = JSON.parse(event.data);
         if (url.indexOf("task") !== -1) {
             // 当前为日程页面
-            const data = JSON.parse(event.data);
-            console.log("event123: ", event);
-            console.log("data123: ", data);
-            console.log("originalTaskList123: ", originalTaskList);
             if (data.msgName === "TASK_START") {
+                // 任务开始
                 const task = originalTaskList.find(task => task.uuid === data?.data?.task?.uuid);
                 const queryData = {...task};
                 if (task && task.uuid) {
@@ -45,6 +49,7 @@ function connectWebSocket(host, port, defaultChannelId) {
                     httpUpdateTask(queryData, false, loadTaskLists(false));
                 }
             } else if (data.msgName === "TASK_STOP") {
+                // 任务结束
                 const task = originalTaskList.find(task => task.uuid === data?.data?.uuid);
                 const queryData = {...task};
                 if (task && task.uuid) {
@@ -54,9 +59,29 @@ function connectWebSocket(host, port, defaultChannelId) {
             } else if (data.msgName === "TASK_SYNC_STATUS_GET_REPLY") {
                
             }
+        } else if (url.indexOf("things") !== -1) {
+            console.log("allThingsListWebsocket: ", allThingsListWebsocket);
+            // 当前为分区设备、或设备页面
+            const deviceName = data.data?.device_name;
+            const newDeviceInfo = data.data?.info;
+            //检查设备信息是否更新，如果更新了，则更新前端数据
+            if (data.msgName === "DEVICE_INFO_UPDATE" || data.msgName === "DEVICE_INFO_GET_REPLY") {
+               const targetDevice = allThingsListWebsocket.find(device => device.credentials.identity === deviceName);
+               const originalDeviceInfo = targetDevice.metadata["info"] || {};
+               const originalDeviceAliase = targetDevice.metadata["aliase"] || "";
+               const originalDeviceName = targetDevice.name;
+               if (JSON.stringify(originalDeviceInfo) !== JSON.stringify(newDeviceInfo) || originalDeviceName !== newDeviceInfo.device_aliase) {
+                const queryData = {...targetDevice};
+                queryData.metadata["info"] = {...newDeviceInfo};
+                queryData.name = newDeviceInfo.device_aliase;
+                queryData.metadata["aliase"] = originalDeviceAliase.replace(originalDeviceName, newDeviceInfo.device_aliase);
+                httpUpdateThingWebsocket(queryData);
+               }
+            }
         }
     };
 
+    // 监听WebSocket的close事件，连接断开时触发
     ws.onclose = function (event) {
         if (event.wasClean) {
             console.log("WebSocket closed cleanly, code=" + event.code + " reason=" + event.reason);
@@ -67,62 +92,129 @@ function connectWebSocket(host, port, defaultChannelId) {
         // 可以在这里尝试重新连接
     };
 
+    // 监听WebSocket的error事件，连接出错时触发重连
     ws.onerror = function (error) {
         console.error("WebSocket Error: ", error);
         connectWebSocket(host, port, defaultChannelId);
     };
 }
 
-function getChannelsAndSendMessage(ip) {
-    let domID = "";
-    if (userInfoStr) {
-        const userInfo = JSON.parse(userInfoStr);
-        domID = userInfo.metadata.domID;
+ // 获取当前domain的ID号
+function httpGetDomainIdWebsocket() {
+    const domainID = sessionStorage.getItem("domainID");
+    if (domainID) {
+        fetch(`/ui/domains/${domainID}/domainInJSON`, {
+            method: "GET",
+          })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json(); // 直接将流转换为JSON对象
+            })
+            .then(json => {
+              const data = json.data;
+              const domainData = JSON.parse(data).domainData;
+              const defaultChannelId = domainData.metadata["comID"];
+              const port =  sessionStorage.getItem("socketBridgePort") || "63001";
+              if (domainID && defaultChannelId) {
+                connectWebSocket(hostNameWebsocket, port, defaultChannelId);
+                httpGetAllThingsListWebsocket(hostNameWebsocket, defaultChannelId);
+              }
+            })
     }
-    
-    fetch(`/ui/channels/channelsInJSON?page=1&limit=1000`, {
+}
+
+// 获取所有设备列表
+function httpGetAllThingsListWebsocket(host, comID) {
+    fetch(`/ui/things/thingsInJSON?page=1&limit=1000&onlineStatus=2`, {
         method: "GET",
     })
-        .then((response) => {
+        .then(response => {
             if (!response.ok) {
-                throw new Error("Network response was not ok");
+                throw new Error('Network response was not ok');
             }
             return response.json(); // 直接将流转换为JSON对象
         })
-        .then((json) => {
+        .then(json => {
             const data = json.data;
-            const channelsData = JSON.parse(data).channelsData;
-            const channels = channelsData.groups;
-            const topics = channels.map((channel) => `channels/${channel.id}/messages`);
-            const message = { topics: topics.join(";"), host: hostName, thingSecret: "platform"+domID, message: "connect" };
-            ws.send(JSON.stringify(message));
+            const thingsData = JSON.parse(data).thingsData;
+            allThingsListWebsocket = thingsData && typeof thingsData === 'object' && 
+                thingsData.things && typeof thingsData.things === 'object' ?  
+                    [...thingsData.things]  : [];
+            if (allThingsListWebsocket.length) {
+                const url = window.location.href;
+                if (url.indexOf("things") !== -1) {
+                    // 进入设备页面时，发送请求获取设备信息
+					controlDeviceWebsocket(host, comID, "deviceInfoGet");
+                }
+            }
+        })
+        .catch(error => {
+            allThingsListWebsocket = [];
         });
 }
+
+function controlDeviceWebsocket(host, comID, controlType) {
+    const out_channel_unique_array = [...new Set(allThingsListWebsocket.map((item) => item.credentials.identity.split('_')[0]))];
+    let queryData = {
+        channelID: comID,
+        host: host,
+        comID: comID,
+        controlType: controlType,
+        uuid: "",
+        username: userInfoWebsocket.credentials.identity,
+        task: {},
+    };
+    async function processRequests() {
+        try {
+            const promises = out_channel_unique_array.map(async (thingIdentity) => {
+                const url = `${protocolWebsocket}//${hostNameWebsocket}:${portSocketBridge}/controlDevice`;
+                const data = {...queryData, thingIdentity: thingIdentity};
+                const response = await fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                });
+                return await response.json();
+            });
+            // 等待所有 Promise 完成（无论成功与否）
+            const results = await Promise.allSettled(promises);
+            // 处理所有的结果
+            results.forEach((result) => {
+                if (result.status === 'fulfilled') {
+                    console.log('成功:', result.value);
+                } else if (result.status === 'rejected') {
+                    console.error('失败:', result.reason);
+                }
+            });
+        } catch (error) {
+            // 捕获意外错误（理论上不会走到这里，因为所有的 Promise 都已经处理）
+            console.error("意外错误:", error);
+        }
+    }
+
+    // 调用异步函数
+    processRequests();
+}
+
+// 更新设备信息
+function httpUpdateThingWebsocket(queryData) {
+    fetch(`/ui/things/${queryData.id}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(queryData),
+    });
+}
+
 
 $(function () {
     // DOM 加载完成后执行的代码
     if (!containsLoginInUrl()) {
-        // 获取当前domain的ID号
-        const domainID = sessionStorage.getItem("domainID");
-        if (domainID) {
-            fetch(`/ui/domains/${domainID}/domainInJSON`, {
-                method: "GET",
-              })
-                .then(response => {
-                  if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                  }
-                  return response.json(); // 直接将流转换为JSON对象
-                })
-                .then(json => {
-                  const data = json.data;
-                  const domainData = JSON.parse(data).domainData;
-                  const defaultChannelId = domainData.metadata["comID"];
-                  const port =  sessionStorage.getItem("socketBridgePort") || "63001";
-                  if (domainID && defaultChannelId) {
-                    connectWebSocket(hostName, port, defaultChannelId);
-                  }
-                })
-        }
+        httpGetDomainIdWebsocket();
     }
 });
